9b408cb55e99e41245d156b04aa47a0b337fcf83
Jeff Asmah <jeff.k.asmah@outlook.com>
2023-12-25 15:52:01 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.cloudinary.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
