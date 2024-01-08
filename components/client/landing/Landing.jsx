168a971c312e1d5898bb7ec9f057c05d9d852fc4
jayCde <jeff.k.asmah@outlook.com>
2024-01-08 12:12:18 +0000

    UX: Clean up landing

--- components/client/landing/Landing.jsx
+++ components/client/landing/Landing.jsx
@@ -5,30 +5,32 @@ import AboutSummarySection from "./subs/aboutSummary";
 import CareerAdsSection from "./subs/careerAds";
 import FeaturedTasksSection from "./subs/featuredTasks";
 import TestimonialBriefSection from "./subs/testimonialBrief";
+import Head from "next/head";
 
 export default function Landing() {
   return (
-    <div className={styles.landingContainer}>
-      <div className={styles.carousel} id="carousel">
-        <h2 className="">LIFE MADE EASIER</h2>
-        <p>
-          <span className={styles.slogan}>
-            Ready to go from <i>To Do </i>to <i>Done</i>?
-          </span>
-          <span className={styles.description}>
-            {" "}
-            We provide instant quotes for most of our residential and commercial
-            services
-          </span>
-        </p>
-        <Link href={"/services/all-services"} className={styles.gotoServices}>
-          Explore
-        </Link>
+    <>
+      <div className={styles.landingContainer}>
+        <div className={styles.carousel} id="carousel">
+          <h2 className="">LIFE MADE EASIER</h2>
+          <p>
+            <span className={styles.slogan}>
+              Ready to go from <i>To Do </i>to <i>Done</i>?
+            </span>
+            <span className={styles.description}>
+              We provide instant quotes for most of our residential and
+              commercial services
+            </span>
+          </p>
+          <Link href={"/services/all-services"} className={styles.gotoServices}>
+            Explore
+          </Link>
+        </div>
+        <TestimonialBriefSection styles={styles} />
+        <FeaturedTasksSection styles={styles} />
+        <AboutSummarySection styles={styles} />
+        <CareerAdsSection styles={styles} />
       </div>
-      <TestimonialBriefSection styles={styles} />
-      <FeaturedTasksSection styles={styles} />
-      <AboutSummarySection styles={styles} />
-      <CareerAdsSection styles={styles} />
-    </div>
+    </>
   );
 }
