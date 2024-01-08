59d7ab23f7415c7b35518175bae9e574eb331cd4
jayCde <jeff.k.asmah@outlook.com>
2024-01-08 12:11:45 +0000

    Logic/UX: Modified user footer sections

--- components/client/Globals/Footer.jsx
+++ components/client/Globals/Footer.jsx
@@ -1,34 +1,57 @@
 import Link from "next/link";
 import { BsFacebook, BsWhatsapp, BsInstagram } from "react-icons/bs";
+import { GetServicesHook } from "../../utils/hooks/serviceMgmtHook";
+import { useState } from "react";
 export default function Footer() {
+  //get some parent services for pupular services section
+  const [popularServices, setPopularServices] = useState([]);
+
+  //--get main service <--> request--//
+  const onError = (response) => {
+    console.log("error", response);
+  };
+
+  const onSuccess = (response) => {
+    const includedServices = response?.data?.result?.filter((service) => {
+      return !service.service_name?.includes(
+        "Taskmann Web" || "Taskmann web exclusive"
+      );
+    });
+    setPopularServices(includedServices?.splice(0, 5));
+  };
+
+  const { isLoading, isError, isSuccess, error } = GetServicesHook(
+    onSuccess,
+    onError
+  );
   return (
     <div className="footer">
       <div className={"footer-sections"}>
         <div className="col">
           <h4>Services</h4>
           <div className={"footerLinkGroup"}>
+            <Link href={"/services/all-services"} className="footerLinkItem">
+              All Services
+            </Link>
             <Link
               href={"/services/all-service-pricings"}
               className="footerLinkItem"
             >
               Pricing
             </Link>
-            <Link href={"/services/all-services"} className="footerLinkItem">
-              All Services
-            </Link>
-            <Link href={"/company/faq"} className="footerLinkItem">
-              FAQ
-            </Link>
           </div>
         </div>
         <div className="col">
           <h4 className="company">Company</h4>
           <div className={"footerLinkGroup"}>
+            <Link href={"/company/contact-us"} className="footerLinkItem">
+              Contact Us
+            </Link>
+            <Link href={"/company/reviews"} className="footerLinkItem">
+              Our Reviews
+            </Link>
             <Link href={"/company/about"} className="footerLinkItem">
               Overview
-            </Link>
-            <Link href={"/company/safety"} className="footerLinkItem">
-              Safety
             </Link>
             <Link
               href={"/company/legal-policies-terms"}
@@ -36,27 +59,27 @@ export default function Footer() {
             >
               Policies
             </Link>
-
-            <Link href={"/company/reviews"} className="footerLinkItem">
-              Our Reviews
-            </Link>
-            <Link href={"/company/contact-us"} className="footerLinkItem">
-              Contact Us
+            <Link href={"/company/safety"} className="footerLinkItem">
+              Safety
             </Link>
           </div>
         </div>
         <div className="col">
           <h4 className="popular-categories">Popular Categories</h4>
           <div className={"footerLinkGroup"}>
-            <Link href={"/"} className="footerLinkItem">
-              Moving
-            </Link>
-            <Link href={"/"} className="footerLinkItem">
-              Car Wash
-            </Link>
-            <Link href={"/"} className="footerLinkItem">
-              Furniture Services
-            </Link>
+            {popularServices?.length > 0 ? (
+              <>
+                {popularServices?.map((pop, _idx) => (
+                  <Link key={_idx + 1} href={"/"} className="footerLinkItem">
+                    {pop?.service_name}
+                  </Link>
+                ))}
+              </>
+            ) : (
+              <Link href={"/services/all-services"} className="footerLinkItem">
+                Head to all services
+              </Link>
+            )}
           </div>
         </div>
         <div className="col">
@@ -67,6 +90,9 @@ export default function Footer() {
             </Link>
             <Link href={"/about/careerpage"} className="footerLinkItem">
               Careers
+            </Link>
+            <Link href={"/company/faq"} className="footerLinkItem">
+              FAQ
             </Link>
             <Link href={"/usage-and-more"} className="footerLinkItem">
               Usage & More
